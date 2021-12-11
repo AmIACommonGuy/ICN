d02108c5d6cc8678810cb5ae1112dcc21fdb2841
Leah Wang <leahwang19@gmail.com>
2021-12-10 20:39:09 -0500

    completed KLtest.R function

--- R/KLtest.R
+++ R/KLtest.R
@@ -1,13 +1,26 @@
-# s: distribution to test (dimension or vector? )
-# null: vector to sample null distributions
-# true: distribution to compare with
-KLtest = function(s, null, true_dist, a, width){
+#' KL test
+#' 
+#' `KLtest` Calculates the KL divergence between G(V_c, V_c') and G_R
+#' matrices 
+#' 
+#' @param s G(Vc, Vc') matrix distribution to test
+#' @param null vector to sample null distributions (ALL singleton edge weights)
+#' @param true_dist Singleton matrix
+#' @param a Significant level alpha. Default is 0.05
+#' @param width maxinum number of nearest neighbors to search (k)
+#' @return Returns `A` Boolean, 1 if KL-divergence significant (C and C' connected) 
+#' and 0 if divergence not significant. 
+#' @examples
+#' 
+#' @export
+
+KLtest = function(s, null, true_dist, a = 0.05, width){
   # All s, null, true are row vectors
-  s_kl = KL.divergence(s,true_dist,width)
+  s_kl = KL.divergence(s, true_dist) #, width)
   t_kl = rep(NA, 1000)
   
   for (i in 1:1000){
-    t = sample(null, length(s), true)
+    t = sample(null, length(s), TRUE)
     t_kl[i] = KL.divergence(t, true_dist, width)
   }
   
@@ -17,5 +30,6 @@ KLtest = function(s, null, true_dist, a, width){
   } else {
     A = 0
   }
+  return(A)
 }
 
